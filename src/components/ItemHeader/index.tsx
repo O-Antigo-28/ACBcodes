63a0854c777d4d8d9b71d8e7fad1c9b9c7e9ffa5
O_Antigo_28 <dadopelapista28@hotmail.com>
2024-11-19 18:37:05 -0300

    adicionado estilos ao componente ItemHeader por uma importação .css

--- src/components/ItemHeader/index.tsx
+++ src/components/ItemHeader/index.tsx
@@ -1,5 +1,6 @@
 import Item from "../Item"
 import { IItemHeader } from "./IItemHeader"
+import "./itemheader.css"
  const ItemHeader: React.FC<IItemHeader> = ({children, subtitle}) => { 
     return (
   
