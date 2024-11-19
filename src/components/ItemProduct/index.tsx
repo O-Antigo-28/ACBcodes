6020adaa4aafb644da13d9d3649501ed91c20de3
O_Antigo_28 <dadopelapista28@hotmail.com>
2024-11-19 18:41:21 -0300

    adicionado estilos ao componente ItemProduct  por uma importação .css e modificado e esqueleto do componente trocando a ordem de exposição das propriedades do produto

--- src/components/ItemProduct/index.tsx
+++ src/components/ItemProduct/index.tsx
@@ -1,13 +1,14 @@
 import List from "../List"
 import Item from "../Item"
 import { IItemProduct } from "./IItemProduct"
+import "./ItemProduct.css"
 const ItemProduct : React.FC<IItemProduct> = ({barcode, name, measure}) => { 
     return(
         <Item className="ItemProduct">
-            
-            <List className="List HorizontalList">
-                <Item>{barcode}</Item>
+        
+            <List style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr"}}>
                 <Item>{name}</Item>
+                <Item style={{fontWeight: 500}}>{barcode}</Item>
                 <Item>{measure}</Item>
             </List>
         
